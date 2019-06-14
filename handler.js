cde007f0b8c423dda44f4b4a982d7c62388d8b43
Kosta Korenkov <kosta@whoot.me>
2019-06-14 16:46:09 +0300

    fix: change the lambda to propagate runtime errors on the client side.
    Otherwise, it just times out on runtime error because callback is never called

--- handler.js
+++ handler.js
@@ -2,22 +2,22 @@
 
 const fetch = require('node-fetch');
 
-module.exports.issues = (event, context, callback) => {
+module.exports.issues = async (event) => {
   const reposUrl = 'https://api.github.com/repos/leapdao/';
-  fetch(`${reposUrl}${event.path.repo}/issues?labels=${event.query.labels}`, {
-    headers: {
-      'Authorization': `token ${process.env.API_TOKEN}`
-    }
-  })
-  .then(response => response.json())
-  .then(data => {
-    const response = {
-      statusCode: 200,
+  const response = await fetch(
+    `${reposUrl}${event.path.repo}/issues?labels=${event.query.labels}`,
+    {
       headers: {
-        'Access-Control-Allow-Origin': '*'
-      },
-      body: data
-    };
-    callback(null, response);
+        'Authorization': `token ${process.env.API_TOKEN}`
+      }
   });
+
+  const data = await response.json();
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: data
+  };
 };
